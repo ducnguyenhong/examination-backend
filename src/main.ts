d38b3f7335543ffcf718397fa3fb1c636c100640
autoclickvn <autoclickvn@gmail.com>
2023-03-27 21:49:12 +0700

    Sửa lỗi CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 3456);
 }
 bootstrap();
